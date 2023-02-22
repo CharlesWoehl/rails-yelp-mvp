605fb709f588a5242aa1414b1a3d3ece7f58b297
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2023-02-22 13:55:59 +0100

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # ON A PAS TRICHÉ HAHAHAHAHA
+
+  resources :restaurants, only: [ :index, :show, :new, :create] do
+    resources :reviews, only: [:new, :create]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
-Restaurant.create([{name: "Mama Mia !", address: "Venise la grande place", phone_number: "00000022", category: "italian"}, {name: "Chang aïe", address: "Paris 14eme", phone_number:  "0101012233", category:  "chinese"},
- {name: "Fujimata", address: "Loin d'ici", phone_number:  "123",  category: "japanese"}, {name: "Chez Ernest", address: "Rennes 35000", phone_number:  "0201012233",  category: "french"}, {name: "Une fois !", address: "rue de la patate", phone_number:  "33222", category: "belgian"}])
+# Restaurant.destroy_all
+# Restaurant.create([{name: "Mama Mia !", address: "Venise la grande place", phone_number: "00000022", category: "italian"}, {name: "Chang aïe", address: "Paris 14eme", phone_number:  "0101012233", category:  "chinese"},
+#  {name: "Fujimata", address: "Loin d'ici", phone_number:  "123",  category: "japanese"}, {name: "Chez Ernest", address: "Rennes 35000", phone_number:  "0201012233",  category: "french"}, {name: "Une fois !", address: "rue de la patate", phone_number:  "33222", category: "belgian"}])
+Review.create([{rating: 5, content: "Un vrai régal", restaurant_id: "1"}, {rating: 3, content: "Bof", restaurant_id: "1"}, {rating: 5, content: "Un vrai régal", restaurant_id: "3"}, {rating: 4, content: "Excquis", restaurant_id: "3"}])
